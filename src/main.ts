14f4bf63019904bd1cab9c5ff4927fdd5ccb11c5
구자현 <110030247+levhyun@users.noreply.github.com>
2024-03-15 23:06:23 +0900

    :sparkles: Add cors config.
    Cors 처리 진행하였다.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvService } from './global/env/env.service';
+import { CorsConfig } from './global/config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(CorsConfig)
 
   await app.listen(EnvService.getPort())
 }
