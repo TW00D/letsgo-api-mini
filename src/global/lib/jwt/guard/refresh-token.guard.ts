278002bd18e5337f33e75a4a2b8433fe0f57be27
구자현 <110030247+levhyun@users.noreply.github.com>
2024-03-16 14:59:40 +0900

    :sparkles: Add token guard.
    토큰 Guard 추가하였다,

---
+++ src/global/lib/jwt/guard/refresh-token.guard.ts
@@ -0,0 +1,11 @@
+import { ExecutionContext, Injectable } from "@nestjs/common"
+import { AuthGuard } from "@nestjs/passport"
+import { Observable } from "rxjs"
+
+
+@Injectable()
+export class RefreshTokenGuard extends AuthGuard('refresh-token') {
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        return super.canActivate(context)
+    }
+}
