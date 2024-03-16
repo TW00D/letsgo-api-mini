604f4bc6e2173c4818193778402c3601b98a5fe4
구자현 <110030247+levhyun@users.noreply.github.com>
2024-03-16 14:08:26 +0900

    :sparkles: Add token DTO.
    토큰 DTO를 작성했다.

---
+++ src/global/lib/jwt/dto/token.dto.ts
@@ -0,0 +1,13 @@
+export class TokenResponse {
+
+    access_token: string;
+
+    refresh_token: string;
+
+    static of(data: any): TokenResponse {
+        return {
+            access_token: data.access_token,
+            refresh_token: data.refresh_token
+        }
+    }
+}   
