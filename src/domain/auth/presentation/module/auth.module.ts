f6d3bfcd2ae1c8b151625088621c05e9aa2579ba
구자현 <110030247+levhyun@users.noreply.github.com>
2024-03-16 15:25:27 +0900

    :sparkles: Add token service in auth module.
    토큰 서비스와 토큰 전략을 Auth 모듈에 추가하였다.

--- src/domain/auth/presentation/module/auth.module.ts
+++ src/domain/auth/presentation/module/auth.module.ts
@@ -4,10 +4,13 @@ import { AuthService } from '../../service/auth.service';
 import { PrismaService } from 'prisma/service/prisma.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { TokenService } from 'src/global/lib/jwt/service/token.service';
+import { AccessTokenStrategy } from 'src/global/lib/jwt/strategy/access-token.strategy';
+import { RefreshTokenStrategy } from 'src/global/lib/jwt/strategy/refresh-token.strategy';
 
 @Module({
     controllers: [AuthController],
-    providers:[AuthService,PrismaService],
+    providers:[AuthService,PrismaService,TokenService,AccessTokenStrategy,RefreshTokenStrategy],
     imports:[
         PassportModule,
         JwtModule.register({})
