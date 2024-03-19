fb005a79c9591b1250168d42d42386e738461b8a
구자현 <110030247+levhyun@users.noreply.github.com>
2024-03-20 08:13:20 +0900

    refac: 로그인DTO 유효성 제거
    로그인 요청 DTO 프로포티의 유효성을 제거하였다.
    
    related: #5

--- src/domain/auth/presentation/dto/login.dto.ts
+++ src/domain/auth/presentation/dto/login.dto.ts
@@ -5,13 +5,13 @@ import { ValidValue } from "src/global/validator/valid.value";
 
 export class LoginRequest {
 
-    @Length(ValidValue.USERNAME_LENGTH_MIN,ValidValue.USERNAME_LENGTH_MAX,{message: ValidMessageConstants.USERNAME_LENGTH})
-    @IsString({message: ValidMessageConstants.USERNAME_STRING})
+    // @Length(ValidValue.USERNAME_LENGTH_MIN,ValidValue.USERNAME_LENGTH_MAX,{message: ValidMessageConstants.USERNAME_LENGTH})
+    // @IsString({message: ValidMessageConstants.USERNAME_STRING})
     @IsNotEmpty({message: ValidMessageConstants.USERNAME_NOT_EMPTY})
     readonly username: string
 
-    @Length(ValidValue.PASSWORD_LENGTH_MIN,ValidValue.PASSWORD_LENGTH_MAX,{message: ValidMessageConstants.PASSWORD_LENGTH})
-    @IsString({message: ValidMessageConstants.PASSWORD_STRING})
+    // @Length(ValidValue.PASSWORD_LENGTH_MIN,ValidValue.PASSWORD_LENGTH_MAX,{message: ValidMessageConstants.PASSWORD_LENGTH})
+    // @IsString({message: ValidMessageConstants.PASSWORD_STRING})
     @IsNotEmpty({message: ValidMessageConstants.PASSWORD_NOT_EMPTY})
     readonly password: string
 }
