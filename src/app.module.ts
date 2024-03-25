b11b0e575bc75f4e767d68d78c47b78126ae1f13
재명 <ljm9894@dgsw.hs.kr>
2024-03-25 20:13:43 +0900

    feat: 카테고리 module 연결
    category Module을 app module에 연결했다.
    
    related: #18

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { EnvConfig } from './global/config/env.config';
 import { LoggerMiddleware } from './global/middleware/logger.middleware';
 import { AuthModule } from './domain/auth/presentation/module/auth.module';
+import { CategoryModule } from './domain/category/presentation/module/category.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(EnvConfig), AuthModule],
+  imports: [ConfigModule.forRoot(EnvConfig), AuthModule, CategoryModule],
   controllers: [],
   providers: [],
 })
