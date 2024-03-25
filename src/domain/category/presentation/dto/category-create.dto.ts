0da28d4edcc645cd66187b5263ef036ca06856a8
재명 <ljm9894@dgsw.hs.kr>
2024-03-25 20:15:46 +0900

    fix: 카테고리 생성 dto 수정
    category create dto에서 isEmpty를 isNotEmpty로 수정했다.
    
    related: #18

--- src/domain/category/presentation/dto/category-create.dto.ts
+++ src/domain/category/presentation/dto/category-create.dto.ts
@@ -1,8 +1,8 @@
-import { IsEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { ValidMessageConstants } from 'src/global/static/valid-message.constants';
 
-export class CreateRequest {
+export class CreateCategoryRequest {
   @IsString({ message: ValidMessageConstants.CATEGORY_STRING })
-  @IsEmpty({ message: ValidMessageConstants.CATEGORY_NOT_EMPTY })
+  @IsNotEmpty({ message: ValidMessageConstants.CATEGORY_NOT_EMPTY })
   nickname: string;
 }
