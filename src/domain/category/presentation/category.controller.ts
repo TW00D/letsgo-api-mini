43b2c1ee7907beece4a6316d450c32cf0fc36569
재명 <ljm9894@dgsw.hs.kr>
2024-03-25 20:41:30 +0900

    feat: Category Controller 카테고리 목록 조회 추가
    카테고리 목록 조회 컨트롤러 추가했다.
    
    related: #19

--- src/domain/category/presentation/category.controller.ts
+++ src/domain/category/presentation/category.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { CreateCategoryRequest } from './dto/category-create.dto';
 import { CategoryService } from '../service/category.service';
 import { GeneralResponse } from 'src/global/response/dto/response.dto';
 import { ReasonPhrases } from 'http-status-codes';
+import { Category } from '../domain/category.model';
 
 @Controller('')
 export class CategoryController {
@@ -18,5 +19,14 @@ export class CategoryController {
       message: ReasonPhrases.OK,
       data: true,
     });
+  }
+  @Get('/category')
+  async getAll() : Promise<GeneralResponse>{
+    const result : Category[] | null = await this.categoryService.getAll();
+    return GeneralResponse.of({
+        code: HttpStatus.OK,
+        message: ReasonPhrases.OK,
+        data : result
+    })
   }
 }
