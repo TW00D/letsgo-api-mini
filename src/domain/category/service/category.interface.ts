06ace1e5b425da8b25b021b7ac3bb12711229e82
재명 <ljm9894@dgsw.hs.kr>
2024-03-25 20:21:32 +0900

    fix: 카테고리 interface 수정
    category 생성 dto class 이름을 수정해서 변경해주었다.
    category 수정 dto class 이름을 수정해서 변경해주었다.
    
    related: #18

--- src/domain/category/service/category.interface.ts
+++ src/domain/category/service/category.interface.ts
@@ -1,11 +1,11 @@
 import { Category } from '../domain/category.model';
-import { CreateRequest } from '../presentation/dto/category-create.dto';
-import { UpdateRequest } from '../presentation/dto/category-update.dto';
+import { CreateCategoryRequest } from '../presentation/dto/category-create.dto';
+import { UpdateCategoryRequest } from '../presentation/dto/category-update.dto';
 
 export interface CategoryInterface {
-  create(request: CreateRequest): Promise<boolean>;
+  create(request: CreateCategoryRequest): Promise<boolean>;
   readAll(): Promise<Category[]>;
   read(id: number): Promise<Category>;
-  update(id: number, request: UpdateRequest): Promise<boolean>;
+  update(id: number, request: UpdateCategoryRequest): Promise<boolean>;
   delete(id: number): Promise<boolean>;
 }
