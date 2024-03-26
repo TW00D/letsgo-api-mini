49d06f2f79c4d696abb1971b6aa17d49b5751a58
Haze Ku <110030247+levhyun@users.noreply.github.com>
2024-03-26 20:35:54 +0900

    Update category-update.dto.ts

--- src/domain/category/presentation/dto/category-update.dto.ts
+++ src/domain/category/presentation/dto/category-update.dto.ts
@@ -4,5 +4,5 @@ import { ValidMessageConstants } from 'src/global/static/valid-message.constants
 export class UpdateCategoryRequest {
   @IsString({ message: ValidMessageConstants.CATEGORY_STRING })
   @IsNotEmpty({ message: ValidMessageConstants.CATEGORY_NOT_EMPTY })
-  nickname: string;
+  name: string;
 }
