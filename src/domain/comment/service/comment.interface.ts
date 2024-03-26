f88d4c05f4ba40a327e82a2851e358e1de3a2f11
재명 <ljm9894@dgsw.hs.kr>
2024-03-26 16:23:04 +0900

    fix: comment interface 수정
    comment 타입 모델로 재정의했다.
    
    related: #12

--- src/domain/comment/service/comment.interface.ts
+++ src/domain/comment/service/comment.interface.ts
@@ -1,9 +1,10 @@
-import { CreateCommentRequest } from '../presentation/dto/comment-create.dto';
-import { UpdateCommentRequest } from '../presentation/dto/comment-update.dto';
+import { Comment } from '../domain/comment.model';
+import { CreateCommentRequest } from '../presentation/dto/Comment-create.dto';
+import { UpdateCommentRequest } from '../presentation/dto/Comment-update.dto';
 
 export interface CommentInterface {
   create(request: CreateCommentRequest): Promise<boolean>;
-  readAll(user: number, post: number, comment: number): Promise<Comment[]>;
+  readAll(user: number, post: number, Comment: number): Promise<Comment[]>;
   readAllByLike(user: number, category: number): Promise<Comment[]>;
   read(id: number): Promise<Comment>;
   update(id: number, request: UpdateCommentRequest): Promise<boolean>;
