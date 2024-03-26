263f448579afb7bf7061a0049aa39d2a8e584fc7
재명 <ljm9894@dgsw.hs.kr>
2024-03-26 10:39:24 +0900

    fix: 카테고리 생성 dto 수정
    ToModel 추가했다.
    
    related: #18

--- src/domain/category/presentation/dto/category-create.dto.ts
+++ src/domain/category/presentation/dto/category-create.dto.ts
@@ -1,8 +1,16 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 import { ValidMessageConstants } from 'src/global/static/valid-message.constants';
+import { Category } from '../../domain/category.model';
 
 export class CreateCategoryRequest {
   @IsString({ message: ValidMessageConstants.CATEGORY_STRING })
   @IsNotEmpty({ message: ValidMessageConstants.CATEGORY_NOT_EMPTY })
   nickname: string;
+
+  static async ToModel(request: CreateCategoryRequest): Promise<Category> {
+    const category = new Category();
+    category.name = request.nickname;
+
+    return category;
+  }
 }
