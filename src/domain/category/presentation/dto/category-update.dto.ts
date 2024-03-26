eab95744139eca4271c7c6e4b53865b7fc8b709e
Haze Ku <110030247+levhyun@users.noreply.github.com>
2024-03-26 21:15:38 +0900

    Update category-update.dto.ts

--- src/domain/category/presentation/dto/category-update.dto.ts
+++ src/domain/category/presentation/dto/category-update.dto.ts
@@ -5,11 +5,11 @@ import { Category } from '../../domain/category.model';
 export class UpdateCategoryRequest {
   @IsString({ message: ValidMessageConstants.CATEGORY_STRING })
   @IsNotEmpty({ message: ValidMessageConstants.CATEGORY_NOT_EMPTY })
-  nickname: string;
+  name: string;
 
   static async ToModel(request: UpdateCategoryRequest): Promise<Category> {
     const category = new Category();
-    category.name = request.nickname;
+    category.name = request.name;
     return category;
   }
 }
