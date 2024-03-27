6e9e8c0266b0d2d1bfb27dd778e5acdc5112f726
재명 <ljm9894@dgsw.hs.kr>
2024-03-27 21:49:13 +0900

    fix: 게시물 module providers 추가
    UserService 추가했다.
    CategoryService 추가했다.
    
    related: #23

--- src/domain/post/presentation/module/post.module.ts
+++ src/domain/post/presentation/module/post.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'prisma/service/prisma.service';
 import { PostService } from '../../service/post.service';
 import { PostController } from '../post.controller';
+import { UserService } from 'src/domain/user/service/user.service';
+import { CategoryService } from 'src/domain/category/service/category.service';
+import { AccessTokenStrategy } from 'src/global/lib/jwt/strategy/access-token.strategy';
 
 @Module({
   controllers: [PostController],
-  providers: [PostService, PrismaService],
+  providers: [
+    PostService,
+    PrismaService,
+    UserService,
+    CategoryService,
+  ],
 })
 export class PostModule {}
