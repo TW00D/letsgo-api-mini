53790ff56191a05370c9cfc0d64a01f66c0fd185
재명 <ljm9894@dgsw.hs.kr>
2024-03-27 21:51:17 +0900

    fix: 게시물 생성 service 수정
    예외 처리 했다.
    게시물 생성 로직 에러 고쳤다.
    
    related: #23

--- src/domain/post/service/post.service.ts
+++ src/domain/post/service/post.service.ts
@@ -3,24 +3,35 @@ import { PrismaService } from 'prisma/service/prisma.service';
 import { CreatePostRequest } from '../presentation/dto/post-create.dto';
 import { User } from '@prisma/client';
 import { UserNotFoundException } from 'src/domain/user/exception/user-notfound.exception';
+import { UserService } from 'src/domain/user/service/user.service';
+import { CategoryService } from 'src/domain/category/service/category.service';
+import { CategoryNotFoundException } from 'src/domain/category/exception/category-notfound.exception';
+import { Payload } from 'src/global/lib/jwt/dto/payload.dto';
 
 @Injectable()
 export class PostService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly userService: UserService,
+    private readonly categoryService: CategoryService,
+  ) {}
 
   public async create(
-    user: User,
+    payload: any,
     request: CreatePostRequest,
   ): Promise<boolean> {
-      
-    if(!user){
-      throw new UserNotFoundException("hello");
+    const findUser = await this.userService.read(payload.iss);
+    const findCategory = await this.categoryService.read(request.category);
+    if (!findUser) {
+      throw new UserNotFoundException(findUser.username);
     }
-    const postModel = await CreatePostRequest.ToModel(request, {
-      connect: user,
-    });
+
+    if (!findCategory) {
+      throw new CategoryNotFoundException();
+    }
+
     await this.prismaService.post.create({
-      data: postModel,
+      data: await CreatePostRequest.ToModel(request, findUser),
     });
 
     return true;
