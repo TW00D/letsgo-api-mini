f7757a1348b8ab7ff75cda8541d40b7d0e6a6808
재명 <ljm9894@dgsw.hs.kr>
2024-03-28 15:15:37 +0900

    feat: 게시물 목록 조회 service 작성
    게시물 목록 조회 service 작성했다.
    
    related: #24

--- src/domain/post/service/post.service.ts
+++ src/domain/post/service/post.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/service/prisma.service';
 import { CreatePostRequest } from '../presentation/dto/post-create.dto';
-import { User } from '@prisma/client';
 import { UserNotFoundException } from 'src/domain/user/exception/user-notfound.exception';
 import { UserService } from 'src/domain/user/service/user.service';
 import { CategoryService } from 'src/domain/category/service/category.service';
 import { CategoryNotFoundException } from 'src/domain/category/exception/category-notfound.exception';
-import { Payload } from 'src/global/lib/jwt/dto/payload.dto';
+import { SortNameException } from '../exception/sort-name.exception';
+import { Post } from '@prisma/client';
+import { PostResponse } from '../presentation/dto/post-response.dto';
+import { QueryFailedException } from '../exception/query-failed.exception';
 
 @Injectable()
 export class PostService {
@@ -36,4 +38,98 @@ export class PostService {
 
     return true;
   }
+
+  public async readAll(
+    payload: any,
+    order: string,
+  ): Promise<PostResponse[] | undefined> {
+    let posts: Post[] | undefined;
+    let filterPosts: PostResponse[] | undefined;
+    if (!order) {
+      throw new QueryFailedException();
+    }
+    switch (order) {
+      case 'recent':
+        posts = await this.prismaService.post.findMany({
+          orderBy: {
+            created_at: 'desc',
+          },
+          include: {
+            _count: {
+              select: {
+                like: true,
+                comment: true,
+              },
+            },
+            like: {
+              where: {
+                user: { username: payload.iss },
+              },
+            },
+          },
+        });
+        filterPosts = posts.map((post) => {
+          let posting = new PostResponse(post);
+          return posting;
+        });
+
+        break;
+      case 'comments':
+        posts = await this.prismaService.post.findMany({
+          orderBy: {
+            comment: {
+              _count: 'desc',
+            },
+          },
+          include: {
+            _count: {
+              select: {
+                like: true,
+                comment: true,
+              },
+            },
+            like: {
+              where: {
+                user: { username: payload.iss },
+              },
+            },
+          },
+        });
+        filterPosts = posts.map((post) => {
+          let posting = new PostResponse(post);
+          return posting;
+        });
+        break;
+      case 'popular':
+        posts = await this.prismaService.post.findMany({
+          orderBy: {
+            like: {
+              _count: 'desc',
+            },
+          },
+          include: {
+            _count: {
+              select: {
+                like: true,
+                comment: true,
+              },
+            },
+            like: {
+              where: {
+                user: { username: payload.iss },
+              },
+            },
+          },
+        });
+        filterPosts = posts.map((post) => {
+          let posting = new PostResponse(post);
+          return posting;
+        });
+        break;
+      default:
+        throw new SortNameException(order);
+    }
+    return filterPosts;
+  }
+  
 }
