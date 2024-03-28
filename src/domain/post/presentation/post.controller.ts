772db53776a706dad86b9b07c5c9c18ba89621f4
재명 <ljm9894@dgsw.hs.kr>
2024-03-28 15:13:28 +0900

    feat: 게시물 목록 조회 controller 작성
    게시물 목록 조회 controller 작성했다.
    
    related: #24

--- src/domain/post/presentation/post.controller.ts
+++ src/domain/post/presentation/post.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { PostService } from '../service/post.service';
@@ -12,6 +14,9 @@ import { CreatePostRequest } from './dto/post-create.dto';
 import { ReasonPhrases } from 'http-status-codes';
 import { TokenInfo } from 'src/global/decorator/token.decorator';
 import { AccessTokenGuard } from 'src/global/lib/jwt/guard/access-token.guard';
+import { Post as PostModel } from '@prisma/client';
+import { PostResponse } from './dto/post-response.dto';
+import { userInfo } from 'os';
 
 @Controller('/post')
 export class PostController {
@@ -29,6 +34,24 @@ export class PostController {
       code: HttpStatus.CREATED,
       message: ReasonPhrases.CREATED,
       data: true,
+    });
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get('/')
+  @UseGuards(AccessTokenGuard)
+  async readAll(
+    @TokenInfo() userInfo: any,
+    @Query('order') order: string,
+  ): Promise<GeneralResponse> {
+    const posts: PostResponse[] | undefined = await this.postService.readAll(
+      userInfo,
+      order,
+    );
+    return GeneralResponse.of({
+      code: HttpStatus.OK,
+      message: ReasonPhrases.OK,
+      data: posts,
     });
   }
 }
